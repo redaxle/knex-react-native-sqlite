0736cd8c287e2a5974122bf5b74b51362588e688
Denis Bushaev <dez64ru@gmail.com>
2022-06-27 00:00:25 +0400

    implement typescript

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+import knex from 'knex';
+import { DatabaseParams } from 'react-native-sqlite-storage';
+import RNSqliteDialect from './src/dialect'
+
+type Config = Omit<knex['Config'], 'connection'> & {connection: DatabaseParams}
+
+declare module 'knex-react-native-sqlite' {
+  function _knex(config:Config):knex.Knex
+  export {RNSqliteDialect, _knex as knex}
+  export default RNSqliteDialect
+}
